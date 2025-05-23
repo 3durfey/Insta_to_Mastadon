82e37c5f793a161bba3a5c3141a7c02465f1eacf
3durfey <3durfey@gmail.com>
2025-05-23 00:05:38 -0400

    added image

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import { createRestAPIClient } from "masto";
-import "dotenv/config"; // if using ES module syntax
+import "dotenv/config";
+import fs from "node:fs";
 
 const masto = createRestAPIClient({
   url: process.env.URL,
   accessToken: process.env.TOKEN,
 });
 
-const status = await masto.v1.statuses.create({
-  status: "Hello from #mastojs!",
+// Create media from a local file
+const attachment1 = await masto.v2.media.create({
+  file: new Blob([fs.readFileSync("dog.jpeg")]),
+  description: "Some image",
 });
 
-console.log(status.url);
+// Publish!
+const status = await masto.v1.statuses.create({
+  status: "Hello from #mastojs!",
+  visibility: "public",
+  mediaIds: [attachment1.id],
+});
+
+console.log(status);
